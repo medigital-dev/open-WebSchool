14733b2a90dbadb96df981f74416830ed5d91a68
@mesaidlg <mesaidlg@gmail.com>
2022-05-14 18:13:55 +0700

    Init Commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/website/smp2wonosari.sch.id/';
+$config['base_url'] = 'http://localhost/CMS-meSchool/';
 
 /*
 |--------------------------------------------------------------------------
