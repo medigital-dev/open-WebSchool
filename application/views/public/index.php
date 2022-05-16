7b3c1775c7d002915c2e74923ce532c6097ca3f4
@mesaidlg <mesaidlg@gmail.com>
2022-05-16 20:19:42 +0700

    Update Homepage v2

--- application/views/public/index.php
+++ application/views/public/index.php
@@ -9,7 +9,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- fontawesome -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome/all.min.css'); ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome/css/all.min.css'); ?>">
   <!-- Custom CSS -->
   <style>
     section {
@@ -29,10 +29,31 @@
       text-decoration: none;
     }
 
-    .blog-group {
+    /* .blog-group {
       overflow-x: hidden;
       overflow-y: auto;
-      height: 300px;
+      height: 540px;
+      scroll-behavior: smooth;
+    }
+
+    .blog-group .card img {
+      object-fit: cover;
+    } */
+    .featured-blog {
+      height: 330px;
+      object-fit: cover;
+    }
+
+    @media (max-width: 320px) {
+      .icon {
+        font-size: .3rem;
+      }
+    }
+
+    @media (max-width: 700px) {
+      .icon {
+        font-size: .55rem;
+      }
     }
   </style>
   <title>#SMP-Demo</title>
@@ -162,7 +183,7 @@
   </section>
 
   <section id="about">
-    <div class="px-4 bg-light rounded-3">
+    <div class="px-4 bg-dark text-white rounded-3">
       <div class="container py-5">
         <h1 class="display-5 fw-bold mb-4">Sekilas tentang kami</h1>
         <p class="col-md-12 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda temporibus totam debitis libero optio saepe nam dolorum repellendus nisi nobis sunt impedit officiis aspernatur architecto sit itaque aliquid corporis, ea labore vero asperiores dolore. Nam at dolor placeat reiciendis numquam ab ratione doloremque facilis culpa id. Iure, consectetur? Eligendi, tenetur.</p>
@@ -191,62 +212,55 @@
   <section id="recent-blog">
     <div class="px-4">
       <div class="container py-5">
-        <h1 class="display-5 fw-bold mb-4">Postingan terakhir</h1>
+        <h1 class="display-5 fw-bold mb-5">Postingan terakhir</h1>
         <div class="row">
           <div class="col-md-8">
-            <div class="card mb-3">
-              <img src="..." class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="container">
+              <div class="card shadow mb-3">
+                <img src="<?= base_url('assets/global/images/image-sample-post.png'); ?>" class="featured-blog" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
               </div>
             </div>
           </div>
           <div class="col-md-4">
-            <div class="blog-group">
-              <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                  <div class="col-lg-4">
-                    <img src="..." class="img-fluid rounded-start" alt="...">
-                  </div>
-                  <div class="col-lg-8">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                  </div>
+            <div class="list-group shadow">
+              <a href="#!" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                  <h5 class="mb-1">List group item heading</h5>
+                  <small>3 days ago</small>
                 </div>
-              </div>
-              <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                  <div class="col-lg-4">
-                    <img src="..." class="img-fluid rounded-start" alt="...">
-                  </div>
-                  <div class="col-lg-8">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                  </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small>And some small print.</small>
+              </a>
+              <a href="#!" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                  <h5 class="mb-1">List group item heading</h5>
+                  <small class="text-muted">3 days ago</small>
                 </div>
-              </div>
-              <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                  <div class="col-lg-4">
-                    <img src="..." class="img-fluid rounded-start" alt="...">
-                  </div>
-                  <div class="col-lg-8">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                  </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small class="text-muted">And some muted small print.</small>
+              </a>
+              <a href="#!" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                  <h5 class="mb-1">List group item heading</h5>
+                  <small class="text-muted">3 days ago</small>
                 </div>
-              </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small class="text-muted">And some muted small print.</small>
+              </a>
+              <a href="#!" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                  <h5 class="mb-1">List group item heading</h5>
+                  <small class="text-muted">3 days ago</small>
+                </div>
+                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                <small class="text-muted">And some muted small print.</small>
+              </a>
+              <a href="#" class="list-group-item list-group-item-action text-center bg-primary text-white">Selengkapnya</a>
             </div>
           </div>
         </div>
@@ -257,10 +271,118 @@
   <section id="contact">
     <div class="px-4">
       <div class="container py-5">
-        <h1 class="display-5 fw-bold mb-4">Hubuni kami</h1>
+        <h1 class="display-5 fw-bold mb-4 text-center">Hubungi kami</h1>
+        <div class="row">
+          <div class="col mb-4">
+            <a href="https://wa.me/62087839301572" target="_blank" class="text-success text-center text-decoration-none d-flex flex-column">
+              <span class="icon">
+                <i class="fa-brands fa-whatsapp fa-fw fa-6x"></i>
+              </span>
+              <span class="fw-bold">087839301572</span>
+            </a>
+          </div>
+          <div class="col mb-4">
+            <a href="https://t.me/mesaidlg" target="_blank" class="text-info text-center text-decoration-none d-flex flex-column">
+              <span class="icon">
+                <i class="fa-brands fa-telegram fa-fw fa-6x"></i>
+              </span>
+              <span class="fw-bold">@mesaidlg</span>
+            </a>
+          </div>
+          <div class="col mb-4">
+            <a href="https://facebook.com/mesaidlg" target="_blank" class="text-primary text-center text-decoration-none d-flex flex-column">
+              <span class="icon">
+                <i class="fa-brands fa-facebook-messenger fa-fw fa-6x"></i>
+              </span>
+              <span class="fw-bold">@mesaidlg</span>
+            </a>
+          </div>
+          <div class="col mb-4">
+            <a href="mailto:hi@example.com" target="_blank" class="text-warning text-center text-decoration-none d-flex flex-column">
+              <span class="icon">
+                <i class="fa-solid fa-envelope fa-fw fa-6x"></i>
+              </span>
+              <span class="fw-bold">hi@example.com</span>
+            </a>
+          </div>
+          <div class="col mb-4">
+            <a href="tel:+62274000000" target="_blank" class="text-secondary text-center text-decoration-none d-flex flex-column">
+              <span class="icon">
+                <i class="fa-solid fa-square-phone fa-fw fa-6x"></i>
+              </span>
+              <span class="fw-bold">(0274) 000000</span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </section>
+
+  <div class="container border-top">
+    <footer class="pt-5">
+      <div class="row">
+        <div class="col-2">
+          <h5>Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+
+        <div class="col-2">
+          <h5>Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+
+        <div class="col-2">
+          <h5>Section</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          </ul>
+        </div>
+
+        <div class="col-4 offset-1">
+          <form>
+            <h5>Subscribe to our newsletter</h5>
+            <p>Monthly digest of whats new and exciting from us.</p>
+            <div class="d-flex w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Email address</label>
+              <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+              <button class="btn btn-primary" type="button">Subscribe</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-between pt-4 my-2 border-top">
+        <p>© 2022 <a href="<?= base_url(); ?>" target="_blank" class="text-dark text-decoration-none">SMP Demo</a>. Create and Development by <a href="https://me-digital.net" target="_blank" class="text-dark text-decoration-none" title="me-digital.net"><img src="<?= base_url('assets/global/images/favicon.png'); ?>" height="25" alt="Logo me-digital.net"></a></p>
+        <ul class="list-unstyled d-flex">
+          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                <use xlink:href="#twitter"></use>
+              </svg></a></li>
+          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                <use xlink:href="#instagram"></use>
+              </svg></a></li>
+          <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24">
+                <use xlink:href="#facebook"></use>
+              </svg></a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
 
   <!-- Optional JavaScript; choose one of the two! -->
 
