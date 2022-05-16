a82c0cf5bf2a9adfa8d2e8d97c6317ace28502c5
@mesaidlg <mesaidlg@gmail.com>
2022-05-16 13:31:28 +0700

    Update Homepage v2

--- application/views/public/index.php
+++ application/views/public/index.php
@@ -8,19 +8,35 @@
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- fontawesome -->
+  <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome/all.min.css'); ?>">
+  <!-- Custom CSS -->
   <style>
     section {
-      padding-top: 4px;
+      padding-top: 3rem;
+    }
+
+    .feature_icon {
+      width: 150px;
+      height: 150px;
+    }
+
+    .cor .navbar {
+      min-height: 5rem;
+    }
+
+    .icon_link {
+      text-decoration: none;
     }
   </style>
-  <title>Hello, world!</title>
+  <title>#SMP-Demo</title>
 </head>
 
 <body>
   <!-- <section id="navbar"> -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow">
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">#SMP-Demo</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -50,36 +66,34 @@
           </li>
         </ul>
         <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <input class="form-control me-2" type="text" placeholder="Cari postingan" aria-label="Search">
         </form>
       </div>
     </div>
   </nav>
   <!-- </section> -->
-
   <section id="heroes">
-    <div class="container col-xxl-8 px-4 py-5">
+    <div class="container col-xxl-8 px-4 py-3">
       <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div class="col-lg-6">
           <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="https://images.unsplash.com/photo-1509062522246-3755977927d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1232&q=80" class="d-block img-thumbnail img-fluid" alt="...">
+                <img src="<?= base_url('assets/global/images/banner_heroes_1.png'); ?>" class="d-block img-thumbnail img-fluid" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <div class="position-absolute bottom-0 start-50 translate-middle-x">1/3</div>
+                  <div class="position-absolute bottom-0 start-50 translate-middle-x fw-bold"><small>1/3</small></div>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1588072432836-e10032774350?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" class="d-block img-thumbnail img-fluid" alt="...">
+                <img src="<?= base_url('assets/global/images/banner_heroes_2.png'); ?>" class="d-block img-thumbnail img-fluid" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <div class="position-absolute bottom-0 start-50 translate-middle-x">2/3</div>
+                  <div class="position-absolute bottom-0 start-50 translate-middle-x fw-bold"><small>2/3</small></div>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" class="d-block img-thumbnail img-fluid" alt="...">
+                <img src="<?= base_url('assets/global/images/banner_heroes_3.png'); ?>" class="d-block img-thumbnail img-fluid" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <div class="position-absolute bottom-0 start-50 translate-middle-x">3/3</div>
+                  <div class="position-absolute bottom-0 start-50 translate-middle-x fw-bold"><small>3/3</small></div>
                 </div>
               </div>
             </div>
@@ -94,80 +108,62 @@
           </div>
         </div>
         <div class="col-lg-6">
-          <h1 class="display-5 fw-bold lh-1 mb-3">Selamat Datang di Sekolah Demo</h1>
+          <h1 class="display-5 fw-bold lh-1 mb-3">Selamat Datang di SMP Demo</h1>
           <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, itaque aspernatur molestiae sapiente esse eos labore. Dignissimos similique numquam aliquid reiciendis, dolorem corporis laudantium ipsum. Laudantium enim architecto recusandae obcaecati?</p>
           <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <a href="#!" class="btn btn-primary btn-lg px-4 me-md-2">Link Cepat</a>
-            <a href="#!" class="btn btn-success btn-lg px-4">PPDB</a>
-            <a href="#!" class="btn btn-outline-secondary btn-lg px-4">etc</a>
+            <a href="#!" class="btn btn-primary btn-lg px-4">Link</a>
           </div>
         </div>
       </div>
     </div>
   </section>
 
-  <section>
-    <div class="container px-4 py-5" id="featured-3">
-      <h2 class="pb-2 border-bottom">Columns with icons</h2>
+  <section id="feature">
+    <div class="container px-4">
       <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-        <div class="feature col">
-          <div class="feature-icon bg-primary bg-gradient">
-            <svg class="bi" width="1em" height="1em">
-              <use xlink:href="#collection"></use>
-            </svg>
+        <div class="col">
+          <div class="d-flex justify-content-center mb-3">
+            <img src="<?= base_url('assets/global/images/features_icon_1.png'); ?>" class="feature_icon">
           </div>
-          <h2>Featured title</h2>
+          <h2>Title</h2>
           <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-          <a href="#" class="icon-link">
-            Call to action
-            <svg class="bi" width="1em" height="1em">
-              <use xlink:href="#chevron-right"></use>
-            </svg>
+          <a href="#" class="icon_link">
+            Link
           </a>
         </div>
-        <div class="feature col">
-          <div class="feature-icon bg-primary bg-gradient">
-            <svg class="bi" width="1em" height="1em">
-              <use xlink:href="#people-circle"></use>
-            </svg>
+        <div class="col">
+          <div class="d-flex justify-content-center mb-3">
+            <img src="<?= base_url('assets/global/images/features_icon_2.png'); ?>" class="feature_icon">
           </div>
-          <h2>Featured title</h2>
+          <h2>Title</h2>
           <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-          <a href="#" class="icon-link">
-            Call to action
-            <svg class="bi" width="1em" height="1em">
-              <use xlink:href="#chevron-right"></use>
-            </svg>
+          <a href="#" class="icon_link">
+            Link
           </a>
         </div>
-        <div class="feature col">
-          <div class="feature-icon bg-primary bg-gradient">
-            <svg class="bi" width="1em" height="1em">
-              <use xlink:href="#toggles2"></use>
-            </svg>
+        <div class="col">
+          <div class="d-flex justify-content-center mb-3">
+            <img src="<?= base_url('assets/global/images/features_icon_3.png'); ?>" class="feature_icon">
           </div>
-          <h2>Featured title</h2>
+          <h2>Title</h2>
           <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-          <a href="#" class="icon-link">
-            Call to action
-            <svg class="bi" width="1em" height="1em">
-              <use xlink:href="#chevron-right"></use>
-            </svg>
+          <a href="#" class="icon_link">
+            Link
           </a>
         </div>
       </div>
     </div>
   </section>
+
+  <section id="about">
+
+  </section>
+
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
 </body>
 
 </html>
