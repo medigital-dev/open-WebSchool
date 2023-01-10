549b2374046c87d422f3664977a98249a3636ff3
meDigital Development <medigital.net@gmail.com>
2023-01-10 17:32:54 +0700

    Update javascript

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -17,3 +17,22 @@ function runDateNow() {
     };
     return i;
   }
+
+  function fireNotif(icon, pesan){
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+        
+    Toast.fire({
+        icon: icon,
+        title: pesan
+    })
+}
